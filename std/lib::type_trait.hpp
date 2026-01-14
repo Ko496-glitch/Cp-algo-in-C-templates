6d9233163fe96d71559e66d4100fdbaab12f03f2
kartikohlan <kartik7ohlan@gmail.com>
2026-01-14 15:00:17 -0500

    added transformation traits, decya<t>

--- std/lib::type_trait.hpp
+++ std/lib::type_trait.hpp
@@ -35,6 +35,70 @@ struct is_optional<optional<U>>: std::true_type{};
 
 template<typename T>
 inline constexpr bool is_optional_v = is_optional<T>::value;
+
+
+
+#if 0
+ Predicate traits 
+#endif
+  template<typename T1, typename T2>
+    class is_same_t{
+      public:
+        static constexpr bool value = false;
+    };
+  
+  template<typename T>
+    class is_same_t<T,T>{
+      public:
+        static constexpr bool value = true;
+    };
+
+  template<typename T1,typename T2, typename ...T3>
+    class is_same_all{
+      public:
+        static constexpr bool value = is_same_t<T1,T2>::value && is_same_t<T2,Rest...>::value;
+    };  
+
+    template<typename T>
+    using IsSame = typename is_same_t<T>::type;
+    
+    template<typename T1, typename T2,typename... T3>
+    using IsSmallAll = typename is_same_all<T1,T2,T3>::type;
+
+#if 0
+  Transformation traits
+#endif
+
+    template<typename T>
+
+    template<typename T&>
+      struct remove_reference{
+        using type = T;
+      };
+
+    template<typename T&&>
+      struct remove_reference{
+        using type = T;
+      };
+     //add reference  
+    template<typename T>
+      struct add_lreference{
+        using type = T&;
+      };
+    
+    template<typename T>
+      struct add_rreference{
+        using type = T&&;
+      };
+    
+    
+    template<typename T>
+    using LvalueRef = typename add_lreference<T>::type;
+    template<typename T>
+    using RvalueRef = typename add_rreference<T>::type;
+
+
+
 }
 
 
